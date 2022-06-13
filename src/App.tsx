00e5eb14ea82ed24d74e0a90d9f5a515ab845b96
Renan Veronez Drechsler <renanvz@outlook.com>
2022-06-13 23:31:19 +0100

    Setting Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 px-6 h-10 rounded">{props.text ?? 'Default'}</button>
+}
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
 }
 
 export default App
